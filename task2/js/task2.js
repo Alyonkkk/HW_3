7db5160f2cc612264b598e9a5009d3ae7358aebf
alyonkkk <jelena.spelkova@gmail.com>
2024-02-13 07:21:05 +0000

    task2

---
+++ task2/js/task2.js
@@ -0,0 +1,9 @@
+// 2. Проверить есть ли в обьекте определенный ключ и если есть вывести **true**
+
+// первый вариант:
+let car = {
+    model: "Toyota",
+    year: 1987,
+    color: "black"
+  };
+  console.log("model" in car);
